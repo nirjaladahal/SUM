40f0cb9a5e10b83508a80e825a08dd945e4720b4
nirjaladahal <117157711+nirjaladahal@users.noreply.github.com>
2025-08-29 17:53:12 +0545

    first commit

---
+++ test/sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Python program to add two numbers
+
+# take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# add two numbers
+sum = num1 + num2
+
+# display the sum
+print("The sum of", num1, "and", num2, "is:", sum)
